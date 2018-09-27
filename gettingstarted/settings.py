2e294b50095308b32ad65a5254ccf86872d1c4e3
Pawel <p.m.bogdanowicz@gmail.com>
2018-09-26 20:10:15 -0400

    working

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
+
+
+db_from_env = dj_database_url.config(conn_max_age=500, require_ssl=True)
+DATABASES['default'].update(db_from_env)
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
