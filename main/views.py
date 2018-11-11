f9ee338326a9504a304d01e049552c83f3859cd4
Pawel <p.m.bogdanowicz@gmail.com>
2018-11-11 11:47:19 -0500

    c

--- main/views.py
+++ main/views.py
@@ -1,9 +1,15 @@
 from django.views.generic import TemplateView
+from django.shortcuts import redirect
 
 
 class IndexPageView(TemplateView):
     template_name = 'main/index.html'
 
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('accounts:log_in')
+
+        return super().dispatch(request, *args, **kwargs)
 
 class ChangeLanguageView(TemplateView):
     template_name = 'main/change_language.html'
