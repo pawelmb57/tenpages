4acd9350780b423e901b42262b240b8be17700c7
Pawel <p.m.bogdanowicz@gmail.com>
2018-12-07 08:45:14 -0500

    collect static

---
+++ staticfiles/looperassets/javascript/pages/simplemde-demo.min.js
@@ -0,0 +1,2 @@
+"use strict";var simpleMDEDemo={init:function(){this.bindUIActions()},bindUIActions:function(){this.handleSimpleMDE()},handleSimpleMDE:function(){new SimpleMDE({element:$("#simplemde")[0],spellChecker:!1,autosave:{enabled:!0,unique_id:"SimpleMDEDemo"}})}};simpleMDEDemo.init();
+//# sourceMappingURL=../../sourcemaps/pages/simplemde-demo.min.js.map
